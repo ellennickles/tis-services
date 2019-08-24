c9bcc03f57695eca948aa7710aa077d4f917f4b9
ellennickles <enickles@gmail.com>
2019-08-24 15:33:00 -0400

    configure new ending no session timer

--- static/scripts/chatbot_script.js
+++ static/scripts/chatbot_script.js
@@ -1,11 +1,111 @@
+// how exactly do we want to end this? 
+// maybe should end after Eve's last response, so guest is not hanging
+// currently Session End display is set to 1.5 secs, perhaps this should be adjusted to the reading time of the final finale statement...
+
 // 120000 is 2 min
 // 60000 is 1 min
 // 30000 is 30 sec
 // 15000 is 15 sec
 
-var sessionDuration = 15000;
-// var session = true;
-var session;
+var nlpDuration = 10000;
+var nlpStatus;
+var finaleStatus;
+var finale = ["zero", "one", "two"]
+var index = 0;
+var respTime = Math.floor(Math.random() * (3000 - 1000)) + 1000;
+
+// sets session boolean to true
+// retrieves current date and time to print to log
+// starts timer
+function startSession() {
+    nlpStatus = true;
+    let start = getStats();
+    let startStr = "*** Session Begin " + start + " ***";
+    updateLog(startStr);
+
+    setTimeout(() => {
+        nlpStatus = false;
+        finaleStatus = true;
+    }, nlpDuration);
+};
+
+// receives guest input
+// clears guest input field
+// prints guest input to log div
+// prints reponse to log div
+document.querySelector("#typehere").onchange = () => {
+    let inputField = document.querySelector("#typehere");
+    let val = inputField.value;
+
+    inputField.value = "";
+
+    updateLog("> " + val)
+
+    if (nlpStatus) {
+        print_nlpResp(val);
+    }; 
+
+    if (finaleStatus) {
+        print_finaleResp();
+    };
+};
+
+// if nlpStatus is true,
+// when get_nlpResp returns with a response,
+// prints that to log div after a slight delay
+async function print_nlpResp(val) {
+    let nlpResp = await get_nlpResp(val);
+    // let respTime = Math.floor(Math.random() * (3000 - 1000)) + 1000;
+
+    setTimeout(() => {
+        updateLog(nlpResp)
+    }, respTime);
+}
+
+// queries the chatbot model for a response to guest input 
+async function get_nlpResp(val) {
+    let got_nlpResp = await fetch("/response.json?sentence=" +
+        encodeURIComponent(val));
+    let data = await got_nlpResp.json();
+    return data["result"];
+};
+
+// if finaleStatus is true,
+// prints to log div from an array of scripted responsess
+function print_finaleResp() {
+    console.log(finale[index]);
+
+    // let respTime = Math.floor(Math.random() * (3000 - 1000)) + 1000;
+    setTimeout(() => {
+        updateLog(finale[index])
+        index++
+
+        if (index === finale.length) {
+            setTimeout(() => {
+                console.log("session expired")
+                endSession();
+            }, 1500);   
+        };
+
+    }, respTime);
+
+    // keep this here if we want to end after user's input
+    // if (index === finale.length) {
+    //     console.log("session expired")
+    //     endSession();
+    // }
+}
+
+// prints to log div
+function updateLog(str) {
+    if (nlpStatus || finaleStatus) {
+        let objDiv = document.getElementById("log");
+        objDiv.appendChild(paraWithText(str));
+        objDiv.scrollTop = objDiv.scrollHeight;
+    } else {
+        return;
+    }
+};
 
 // packages the string to print to log div
 function paraWithText(t) {
@@ -15,59 +115,9 @@ function paraWithText(t) {
     return ptag;
 };
 
-// if the session is true, then prints to log div
-function updateLog(str) {
-    if (session) {
-        let objDiv = document.getElementById("log");
-        objDiv.appendChild(paraWithText(str));
-        objDiv.scrollTop = objDiv.scrollHeight;
-    } else {
-        return;
-    }
-};
-
-// receives user input
-// passes user input value to the getResp function
-// clears user input field
-// and when getResp returns, prints to log div after a slight delay
-document.querySelector("#typehere").onchange = async function () {
-    let inputField = document.querySelector("#typehere");
-    let val = inputField.value;
-    inputField.value = "";
-    let resp = await getResp(val);
-    updateLog("> " + val)
-    let respTime = Math.floor(Math.random() * (3000 - 1000)) + 1000;
-    setTimeout(() => {
-        updateLog(resp)
-    }, respTime);
-};
-
-// queries the chatbot model for a response to user input 
-async function getResp(val) {
-    let getResp = await fetch("/response.json?sentence=" +
-        encodeURIComponent(val));
-    let data = await getResp.json();
-    return data["result"];
-};
-
-// sets session boolean to true
-// retrieves current date and time to print to log
-// starts timer
-function startSession() {
-    session = true;
-    let start = getStats();
-    let startStr = "*** Session Begin " + start + " ***";
-    updateLog(startStr);
-
-    setTimeout(() => {
-        endSession()
-    }, sessionDuration);
-};
-
-// when timer ends,
-// removes user input field from view
+// removes guest input field from view
 // retrieves the current date and time to print to log div
-// sets the session boolean to false
+// sets the finaleStatus boolean to false
 // prints text at the bottom of document
 function endSession() {
     document.getElementById("typehere").style.display = "none";
@@ -76,11 +126,11 @@ function endSession() {
     let endStr = "*** Session End " + end + " ***";
     updateLog(endStr);
 
-    session = false;
+    finaleStatus = false;
 
     let expired = "Session Expired"
     document.body.appendChild(paraWithText(expired));
-};
+}
 
 // returns the current date and time
 function getStats() {
